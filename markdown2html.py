7be7cf0465f09fe9377fd3b086c3cf30a6180085
Annette-Munyiri <anmunyiri390@gmail.com>
2022-04-26 07:43:54 -0700

    Pycodestyle

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-"""
-Script converts markdown to HTML.
-"""
+# Script converts markdown to HTML.
 
 from html_builder import HTML
 
 if __name__ == '__main__':
 
-        html = HTML()
-        html.process().build().substitute().write()
-        exit(0)
+    html = HTML()
+    html.process().build().substitute().write()
+    exit(0)
